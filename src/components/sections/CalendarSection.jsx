c9d34b03fbbc10314a768f83f0eb145cb38e4518
Martin <martinalturria@hotmail.com>
2025-04-07 14:45:59 -0300

    fix services

--- src/components/sections/CalendarSection.jsx
+++ src/components/sections/CalendarSection.jsx
@@ -5,7 +5,7 @@ const CalendarSection = ({
     textColor = "text-white",
 }) => {
     useEffect(() => {
-        const script = document.createElement('script');
+        const script = document.createElement("script");
         script.src = "https://assets.calendly.com/assets/external/widget.js";
         script.async = true;
         document.body.appendChild(script);
@@ -20,16 +20,16 @@ const CalendarSection = ({
             className={`w-full ${bgColor} py-24 md:py-32 relative flex flex-col items-center justify-center min-h-[550px]`}
         >
             <h2 className={`${textColor} text-xl md:text-5xl text-center mb-8`}>
-                Agenda una Cita
+                Schedule an Appointment
             </h2>
-            <div 
-                className="calendly-inline-widget w-full max-w-[1200px] px-4" 
-                data-url="https://calendly.com/ivan-dlg?hide_landing_page_details=1&hide_gdpr_banner=1" 
+            <div
+                className="calendly-inline-widget w-full max-w-[1200px] px-4"
+                data-url="https://calendly.com/ivan-dlg/30min?hide_landing_page_details=1&hide_gdpr_banner=1&back=1&month=2025-04"
                 style={{
-                    minWidth: '320px', 
-                    height: '700px',
-                    width: '100%',
-                    maxWidth: '1200px'
+                    minWidth: "320px",
+                    height: "700px",
+                    width: "100%",
+                    maxWidth: "1200px",
                 }}
             />
         </section>
--- src/pages/BlogArticle.jsx
+++ src/pages/BlogArticle.jsx
@@ -25,20 +25,20 @@ const BlogArticle = () => {
 
     return (
         <div className="w-full bg-gray flex justify-center py-24">
-            <div className="md:w-11/12 w-full mx-4 md:mx-0 bg-white rounded-2xl shadow-lg px-8 py-12">
-                <h1 className="md:text-4xl text-2xl font-bold mb-4 text-left w-full pb-1 border-b border-black/30">
+            <div className="lg:w-11/12 w-full mx-8 lg:mx-0 bg-white rounded-2xl shadow-lg px-8 py-12">
+                <h1 className="lg:text-3xl md:text-2xl text-xl font-bold mb-4 text-left w-full pb-1 border-b border-black/30">
                     Lorem Ipsum is simply dummy text of the printing
                 </h1>
 
                 <img
                     src="/images/blog-detail.png"
                     alt="Article Image"
-                    className="w-full my-8 h-1/3"
+                    className="w-full my-8 lg:h-1/3 md:h-80 h-60"
                     style={{ borderRadius: "20px" }}
                 />
 
                 <div className="prose max-w-none mb-8">
-                    <p className="md:text-2xl text-lg">
+                    <p className="lg:text-xl md:text-lg text-base leading-normal">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Sed euismod, nisl vel ultricies lacinia, nisl nisl
                         aliquam nisl, eget aliquam nisl nisl sit amet nisl. Sed
@@ -74,7 +74,7 @@ const BlogArticle = () => {
                         lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl
                         sit amet nisl.
                     </p>
-                    <p className="md:text-2xl text-lg">
+                    <p className="lg:text-2xl text-lg">
                         Sed euismod, nisl vel ultricies lacinia, nisl nisl
                         aliquam nisl, eget aliquam nisl nisl sit amet nisl. Sed
                         euismod, nisl vel ultricies lacinia, nisl nisl aliquam
@@ -83,31 +83,31 @@ const BlogArticle = () => {
                 </div>
 
                 <div className="mb-4 mt-10">
-                    <p className="text-gray-500 text-sm md:text-xl">Date</p>
-                    <p className="text-gray-500 mt-8 md:text-xl text-sm">
+                    <p className="text-gray-500 text-sm lg:text-xl">Date</p>
+                    <p className="text-gray-500 mt-8 lg:text-xl text-sm">
                         Categories:
                     </p>
                 </div>
 
                 <div className="flex flex-col space-x-4 mb-16 mt-8">
-                    <p className="text-gray-600 mr-4 md:text-xl text-sm">
+                    <p className="text-gray-600 mr-4 lg:text-xl text-sm">
                         Share:
                     </p>
                     <div className="flex space-x-4 mt-4">
                         <img
                             src="/images/footer-wha.png"
                             alt="WhatsApp"
-                            className="md:h-7 md:w-7 h-5 w-5 cursor-pointer"
+                            className="lg:h-7 lg:w-7 h-5 w-5 cursor-pointer"
                         />
                         <img
                             src="/images/footer-insta.png"
                             alt="Instagram"
-                            className="md:h-7 md:w-7 h-5 w-5 cursor-pointer"
+                            className="lg:h-7 lg:w-7 h-5 w-5 cursor-pointer"
                         />
                         <img
                             src="/images/footer-linke.svg"
                             alt="LinkedIn"
-                            className="md:h-7 md:w-7 h-5 w-5 cursor-pointer"
+                            className="lg:h-7 lg:w-7 h-5 w-5 cursor-pointer"
                         />
                     </div>
                 </div>
@@ -116,7 +116,7 @@ const BlogArticle = () => {
                     Related Articles
                 </h2>
 
-                <div className="grid grid-cols-3 gap-6">
+                <div className="grid grid-cols-3 gap-4">
                     {relatedArticles.map((article, index) => (
                         <Link
                             to={`/blog/${article.id}`}
@@ -127,12 +127,12 @@ const BlogArticle = () => {
                                 <img
                                     src={article.image}
                                     alt={article.title}
-                                    className="md:w-80 md:h-80 w-35 h-35 rounded-lg"
+                                    className="lg:w-70 lg:h-70 w-25 h-25 rounded-lg"
                                 />
-                                <p className="text-gray-500 text-sm md:text-xl">
+                                <p className="text-gray-500 text-sm lg:text-base">
                                     {article.date}
                                 </p>
-                                <h4 className="md:text-2xl text-lg font-medium">
+                                <h4 className="lg:text-xl text-sm font-medium">
                                     {article.title}
                                 </h4>
                             </div>
